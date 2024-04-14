e5f080ec83ad2ebe3c9f3c7a79c77b71339a96a9
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2024-04-14 12:08:59 +0200

    feat: added first blazor web app test with signalr connection. includes fetchprice for cryptocurrencies with coingecko api.

---
+++ code/WebApp/WebApp/PricesHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebApp.Services;
+
+public class PricesHub : Hub
+{
+    private readonly DataFetcherService _dataFetcherService;
+
+    public PricesHub(DataFetcherService dataFetcherService)
+    {
+        _dataFetcherService = dataFetcherService;
+    }
+
+    public async Task SendPriceUpdate(decimal? price)
+    {
+        await Clients.All.SendAsync("ReceivePriceUpdate", price);
+    }
+}
