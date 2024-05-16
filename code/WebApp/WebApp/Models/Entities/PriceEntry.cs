64677f17cda38e64176090663d019271691a40d4
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2024-05-16 13:05:47 +0200

    feat: added wallet page design. still needs correct implemenation. fix: removed unnecessary test code on homepage struc: added entities folder and added entities

---
+++ code/WebApp/WebApp/Models/Entities/PriceEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Models.Entities
+{
+    public class PriceEntry
+    {
+        public string Symbol { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ code/WebApp/WebApp/Models/Entities/WalletEntry.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.Components.Pages;
+
+namespace WebApp.Models.Entities
+{
+    public class WalletEntry
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public decimal? Amount { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
