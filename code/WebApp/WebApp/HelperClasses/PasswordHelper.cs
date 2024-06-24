73c2756faa735f685364b5489f13bdbc4e748b96
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2024-06-24 15:10:30 +0200

    feat: using Argon2id: new Password Hashing algorithm

--- code/WebApp/WebApp/HelperClasses/PasswordHelper.cs
+++ code/WebApp/WebApp/HelperClasses/PasswordHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
+using Konscious.Security.Cryptography;
 
 namespace WebApp.HelperClasses
 {
@@ -7,25 +8,41 @@ namespace WebApp.HelperClasses
     {
         public static (string Hash, string Salt) HashPassword(string password)
         {
+            byte[] saltBytes = new byte[16];
             using (var rng = new RNGCryptoServiceProvider())
             {
-                byte[] saltBytes = new byte[16];
                 rng.GetBytes(saltBytes);
-                string salt = Convert.ToBase64String(saltBytes);
-
-                var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000);
-                byte[] hashBytes = pbkdf2.GetBytes(20);
-                string hash = Convert.ToBase64String(hashBytes);
-
-                return (hash, salt);
             }
+            string salt = Convert.ToBase64String(saltBytes);
+
+            byte[] passwordAsBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            var argon2 = new Argon2id(passwordAsBytes)
+            {
+                Salt = saltBytes,
+                DegreeOfParallelism = 8, // number of threads to use
+                Iterations = 4,
+                MemorySize = 1024 * 64 // 64 MB
+            };
+            byte[] hashBytes = argon2.GetBytes(32);
+            string hash = Convert.ToBase64String(hashBytes);
+
+            return (hash, salt);
         }
 
         public static bool VerifyPassword(string enteredPassword, string storedHash, string storedSalt)
         {
             byte[] saltBytes = Convert.FromBase64String(storedSalt);
-            var pbkdf2 = new Rfc2898DeriveBytes(enteredPassword, saltBytes, 10000);
-            byte[] enteredHashBytes = pbkdf2.GetBytes(20);
+            byte[] enteredPasswordAsBytes = System.Text.Encoding.UTF8.GetBytes(enteredPassword);
+
+            var argon2 = new Argon2id(enteredPasswordAsBytes)
+            {
+                Salt = saltBytes,
+                DegreeOfParallelism = 8,
+                Iterations = 4,
+                MemorySize = 1024 * 64
+            };
+            byte[] enteredHashBytes = argon2.GetBytes(32);
             string enteredHash = Convert.ToBase64String(enteredHashBytes);
 
             return enteredHash == storedHash;
